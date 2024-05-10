fb788a25af41ff851a57cacc64b075104f78a8d2
thebenworrall <120256339+thebenworrall@users.noreply.github.com>
2024-05-10 12:30:01 +0800

    Ben Worrall Website v2.01

--- pages/utils/find_related_posts.js
+++ pages/utils/find_related_posts.js
@@ -1,17 +1,36 @@
 
 const findRelatedPosts = (currentTags, allPosts, currentPostId) => {
+  // Ensure allPosts is an array. If not, default to an empty array.
+  const posts = Array.isArray(allPosts) ? allPosts : [];
 
-    return allPosts
-    .filter(post => post.id !== currentPostId)
-    .map(post => ({
-      ...post,
-      matchCount: post.tags.filter(tag => currentTags.includes(tag.sys.id)).length
-    }))
-    .filter(post => post.matchCount > 0)
-    .sort((a, b) => b.matchCount - a.matchCount)
-    .slice(0, 4)
-    
+  return posts
+      .filter(post => post.id !== currentPostId)
+      .map(post => ({
+          ...post,
+          matchCount: post.tags.filter(tag => currentTags.includes(tag.sys.id)).length
+      }))
+      .filter(post => post.matchCount > 0)
+      .sort((a, b) => b.matchCount - a.matchCount)
+      .slice(0, 4);
 }
 
+export default findRelatedPosts;
 
-export default findRelatedPosts
+//OLD CODE
+
+// const findRelatedPosts = (currentTags, allPosts, currentPostId) => {
+
+//     return allPosts
+//     .filter(post => post.id !== currentPostId)
+//     .map(post => ({
+//       ...post,
+//       matchCount: post.tags.filter(tag => currentTags.includes(tag.sys.id)).length
+//     }))
+//     .filter(post => post.matchCount > 0)
+//     .sort((a, b) => b.matchCount - a.matchCount)
+//     .slice(0, 4)
+    
+// }
+
+
+// export default findRelatedPosts
