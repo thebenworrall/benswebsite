6f35e41d14d3f35eacdf25feac5ae31f8d9a6454
thebenworrall <120256339+thebenworrall@users.noreply.github.com>
2024-06-11 15:02:24 +0800

    Ben Worrall Website v2.08

--- components/newsletter_subscribe.js
+++ components/newsletter_subscribe.js
@@ -40,9 +40,12 @@ const NewsletterSubscribe = () => {
     return (
         <Flex className={classes.letter_signup} flexDirection="column" width="100%" height="280px"> 
             <div className={classes.container}>
-                    <h1 className={classes.h1}> The Newsletter </h1>
+                    <h1 className={classes.h1}> Join the Newsletter </h1>
                     <h2 className={classes.h2}> </h2>
-                    <p className={classes.p}> Don't miss a post. Join my growing mailing list and get new posts sent straight to your inbox.</p>
+                    <p className={classes.p}> Don't miss a post! Be the first to gain access to new articles, stories,
+                                              and exclusive content. </p>
+                
+                    
                     <form className={classes.form} onSubmit={submitHandler}>
                         <input className={classes.input} ref={emailInputRef} placeholder='your email' />
                         <button className={classes.button}> Subscribe </button>
